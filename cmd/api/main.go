1eda0eb5f711c4a76d802475a5693ffbbc6a7eb9
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-01-24 21:36:41 +0330

    Setup postgresql and connect to it.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0" // TODO: generate automatically at build time
@@ -14,6 +19,9 @@ const version = "1.0.0" // TODO: generate automatically at build time
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string // Data Source Name
+	}
 }
 
 type application struct {
@@ -22,13 +30,41 @@ type application struct {
 }
 
 func main() {
+	/* NOTE:
+	 * you should create a .env file at the root of your project and add
+	 * POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_DATABASE, POSTGRES_HOST  to
+	 * it.
+	 */
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbUsername := os.Getenv("POSTGRES_USERNAME")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbHost := os.Getenv("POSTGRES_HOST")
+	dbName := os.Getenv("POSTGRES_DATABASE")
+
 	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(
+		&cfg.db.dsn,
+		"db-dsn",
+		"postgres://"+dbUsername+":"+dbPassword+"@"+dbHost+"/"+dbName,
+		"PostgreSQL DSN",
+	)
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+	logger.Printf("database connection pool established")
+
 	app := &application{
 		config: cfg,
 		logger: logger,
@@ -43,6 +79,20 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/exvimmer/lets_go_further/greenlight
 
 go 1.19
 
-require github.com/julienschmidt/httprouter v1.3.0
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.2
+)
