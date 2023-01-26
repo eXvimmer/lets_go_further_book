23534ecceba83d817c9c87415854e86dce809744
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-01-26 04:59:09 +0330

    Manage SQL query timeouts.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"time"
@@ -47,7 +48,12 @@ func (m *MovieModel) Insert(movie *Movie) error {
     RETURNING id, created_at, version;
   `
 	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
-	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(
+		&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m *MovieModel) Get(id int64) (*Movie, error) {
@@ -60,7 +66,11 @@ func (m *MovieModel) Get(id int64) (*Movie, error) {
     WHERE id = $1;
   `
 	var movie Movie
-	err := m.DB.QueryRow(query, id).Scan( // order matters
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, id).Scan( // order matters
 		&movie.ID,
 		&movie.CreatedAt,
 		&movie.Title,
@@ -96,10 +106,14 @@ func (m *MovieModel) Update(movie *Movie) error {
 		movie.ID,
 		movie.Version,
 	}
-	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.Version)
 	if err != nil {
 		switch {
-		case errors.Is(err, ErrEditConflict):
+		case errors.Is(err, sql.ErrNoRows):
 			return ErrEditConflict
 		default:
 			return err
@@ -117,7 +131,11 @@ func (m *MovieModel) Delete(id int64) error {
     DELETE FROM movies
     WHERE id = $1;
   `
-	res, err := m.DB.Exec(query, id)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	res, err := m.DB.ExecContext(ctx, query, id)
 	if err != nil {
 		return err
 	}
