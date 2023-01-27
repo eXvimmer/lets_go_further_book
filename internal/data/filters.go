a28e42f3dda37ce13d5398d82f93f5ddc4111195
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-01-27 09:17:57 +0330

    Sort lists of movies that the user gets.

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "github.com/exvimmer/lets_go_further/greenlight/internal/validator"
+import (
+	"strings"
+
+	"github.com/exvimmer/lets_go_further/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -15,4 +19,22 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize < 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, v := range f.SortSafelist {
+		if f.Sort == v {
+			return strings.TrimPrefix(v, "-")
+		}
+	}
+	// this shouldn't happen; the Sort value should have already been checked by
+	// calling the ValidateFilters function.
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
