c7bbf81fcbd6f15596701d9f511e011025eddcb6
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-01-28 17:48:14 +0330

    Configure the rate limiters.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -26,6 +26,11 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime  string
 	}
+	limiter struct {
+		rps     float64 // Requests Per Second
+		burst   int
+		enabled bool
+	}
 }
 
 type application struct {
@@ -54,6 +59,11 @@ func main() {
 		"PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m",
 		"PostgreSQL max connection idle time")
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2,
+		"Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
